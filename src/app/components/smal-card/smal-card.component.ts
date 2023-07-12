5305f9a6b5b813a918df93368951c434aa349418
LeonardoSPires <leopires1988@gmail.com>
2023-07-12 13:51:20 -0300

    Tornei dinâmico os components

--- src/app/components/smal-card/smal-card.component.ts
+++ src/app/components/smal-card/smal-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-smal-card',
   templateUrl: './smal-card.component.html',
   styleUrls: ['./smal-card.component.css']
 })
-export class SmalCardComponent {
+export class SmalCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string =""
+  @Input()
+  cardTitle:string =""
+  @Input()
+  cardDescription:string =""
+
+  constructor () {  }
+  ngOnInit() {
+
+
+  }
 
 }
